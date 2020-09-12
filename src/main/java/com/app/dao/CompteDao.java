2d6146a6e89769df1c71f4ca6d3277a12c297c76
YOUSSOUF SOKAMTE <youssoufngounou@gmail.com>
2020-09-12 13:57:40 +0100

    ajout des autorisations

--- src/main/java/com/app/dao/CompteDao.java
+++ src/main/java/com/app/dao/CompteDao.java
@@ -10,12 +10,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author kouam
  */
+
 public interface CompteDao extends JpaRepository<Compte, Long>{
     
     @Query("select cpt from compte cpt where cpt.username like :x")
--- src/main/java/com/app/dao/DaoApplication.java
+++ src/main/java/com/app/dao/DaoApplication.java
@@ -1,10 +1,19 @@
 package com.app.dao;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={
+		"com.app.dao", "com.app.entities"})
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.app.entities, com.app.dao")
+@EnableJpaRepositories(basePackages="com.app.entities, com.app.dao")
+@EnableTransactionManagement
 @EntityScan(basePackages = "com.app.entities, com.app.dao")
 public class DaoApplication {
 
